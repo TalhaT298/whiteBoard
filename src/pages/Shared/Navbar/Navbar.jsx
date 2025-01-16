73bf689a3c61aca4afc1cf5cee430341d1e9c172
talha <mdtalha2008@gmail.com>
2025-01-16 14:07:16 +0600

    logo add

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,29 +1,20 @@
 import React from "react";
-
+import logo from "../../../../src/assets/logo.png"
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar ">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <img src={logo} className="w-24 h-10" alt="" />
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
+      <div className="flex">
+        <ul className="flex">
           <li>
             <a>Link</a>
           </li>
           <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="bg-base-100 rounded-t-none p-2">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
+            <a>Link2</a>
           </li>
+          
         </ul>
       </div>
     </div>
