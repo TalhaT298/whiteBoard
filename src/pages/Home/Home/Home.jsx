79e86ee466dd7780122c442ac39c58d6b7a2ec85
talha <mdtalha2008@gmail.com>
2025-01-17 00:01:19 +0600

    navbar update

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Ball from '../Ball/Ball';
 
+
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Ball></Ball>
+           
         </div>
     );
 };
